6e9ec324f5ea52d8882088d297dc7af75eada4c8
itsumrat <itsumrat@gmail.com>
2016-10-14 14:30:45 +0600

    added new section

--- js/main.js
+++ js/main.js
@@ -46,4 +46,22 @@ $(document).ready(function() {
 		nav.toggleClass('open');
 	});
 
+	// Slider
+	var slider = $(".slider"),
+		slides = slider.find('li'),
+		nav = slider.find('nav');
+
+	slides.eq(0).addClass('current');
+
+	nav.children('a').eq(0).addClass('current_dot');
+
+	nav.on('click', 'a', function(event) {
+		event.preventDefault();
+		$(this).addClass('current_dot').siblings().removeClass('current_dot');
+		slides.eq($(this).index()).addClass('current').removeClass('prev').siblings().removeClass('current');
+		slides.eq($(this).index()).prevAll().addClass('prev');
+		slides.eq($(this).index()).nextAll().removeClass('prev');
+	});
+
+
 });
